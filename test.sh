4412425861b07f0858d0a3475b1d496a004298ff
Eran Rundstein <eran@rundste.in>
2024-02-24 01:04:38 -0800

    iterate2

--- test.sh
+++ test.sh
@@ -2,16 +2,27 @@
 set -ex
 
 docker_build() {
+  echo "Arch: $ARCH"
+  echo "Platform: $PLATFORM"
+
   # NB: add -vv to cargo build when debugging
   local -r crate="$1"crate
   docker run --rm \
     -v "$PWD/test/${crate}:/volume" \
     -v cargo-cache:/root/.cargo/registry \
     -e RUST_BACKTRACE=1 \
-    clux/muslrust:temp \
+    --platform $PLATFORM
+    rustmusl-temp \
     cargo build
+
   cd "test/${crate}"
-  ./target/x86_64-unknown-linux-musl/debug/"${crate}"
+
+  docker run --rm \
+    -v "$PWD:/volume" \
+    -t ubuntu:latest
+    --platform $PLATFORM \
+    /volume/target/$ARCH-unknown-linux-musl/debug/"${crate}"
+
   ldd "target/x86_64-unknown-linux-musl/debug/${crate}" 2>&1 | grep -qE "not a dynamic|statically linked" && \
     echo "${crate} is a static executable"
 }
