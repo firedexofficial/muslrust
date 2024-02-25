5cf5fd9fd0673f35ccf40da0c8d11cff3e79253e
Eran Rundstein <eran@rundste.in>
2024-02-24 23:46:35 -0800

    iterate3

--- test.sh
+++ test.sh
@@ -22,8 +22,7 @@ docker_build() {
     -e RUST_BACKTRACE=1 \
     --platform $PLATFORM \
     ubuntu:latest \
-    bash -c "pwd; ls -la; find .; /volume/target/$TARGET_DIR/debug/${crate}"
-    #/volume/target/$TARGET_DIR/debug/"${crate}"
+    ./volume/target/$TARGET_DIR/debug/"${crate}"
 
   ldd "target/x86_64-unknown-linux-musl/debug/${crate}" 2>&1 | grep -qE "not a dynamic|statically linked" && \
     echo "${crate} is a static executable"
