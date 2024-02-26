8dc2080b81db72dcf7d0e7cdc6bab255e7c2b19e
Eran Rundstein <eran@rundste.in>
2024-02-25 17:24:07 -0800

    iterate3

--- test.sh
+++ test.sh
@@ -22,7 +22,7 @@ docker_build() {
     -e RUST_BACKTRACE=1 \
     --platform $PLATFORM \
     test-runner \
-    bash -c "cd volume; ls -la target; ./target/$TARGET_DIR/debug/${crate} && ldd /volume/target/$TARGET_DIR/debug/${crate} 2>&1 | grep -qE 'not a dynamic|statically linked' && echo ${crate} is a static executable"
+    bash -c "cd volume; ls -la target; ./target/$TARGET_DIR/debug/${crate} && ldd ./target/$TARGET_DIR/debug/${crate} && ldd /volume/target/$TARGET_DIR/debug/${crate} 2>&1 | grep -qE 'not a dynamic|statically linked' && echo ${crate} is a static executable"
 
 
   ldd "target/x86_64-unknown-linux-musl/debug/${crate}" 2>&1 | grep -qE "not a dynamic|statically linked" && \
